cbddbeb7ae52e746e2dc57b32b291f36aa684c5c
WalicFeed <nazar@mikunov.ru>
2026-06-25 00:50:05 +0600

    repo init

---
+++ product/seriallizers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import Product, Category, Review
+
+class ProductListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = 'id title price category'.split()
+        # # fields = '__all__'
+        # exclude = 'id text'.split()
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = 'id name'.split()
+        # # fields = '__all__'
+        # exclude = 'id text'.split()
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class ReviewListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = 'id product'.split()
+        # # fields = '__all__'
+        # exclude = 'id text'.split()
+
+class ReviewDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
---
+++ product/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Product, Category, Review
+from django.forms import model_to_dict
+from . import seriallizers
+from rest_framework import status
+
+@api_view(http_method_names=['GET'])
+def product_list_api_view(request):
+    products = Product.objects.all()
+    data = seriallizers.ProductListSerializer(products, many=True).data
+    return Response(
+        data=data
+    )
+
+@api_view(http_method_names=['GET'])
+def product_detail_api_fiew(request, id):
+    try:
+        product = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        return Response(data={'error': 'Product not found!'}, status=status.HTTP_404_NOT_FOUND)
+    data = seriallizers.ProductDetailSerializer(product, many=False).data
+    return Response(data=data)
+
+@api_view(http_method_names=['GET'])
+def category_list_api_view(request):
+    categories = Category.objects.all()
+    data = seriallizers.CategoryListSerializer(categories, many=True).data
+    return Response(
+        data=data
+    )
+
+@api_view(http_method_names=['GET'])
+def category_detail_api_fiew(request, id):
+    try:
+        category = Category.objects.get(id=id)
+    except Category.DoesNotExist:
+        return Response(data={'error': 'Category not found!'}, status=status.HTTP_404_NOT_FOUND)
+    data = seriallizers.CategoryDetailSerializer(category, many=False).data
+    return Response(data=data)
+
+@api_view(http_method_names=['GET'])
+def review_list_api_view(request):
+    reviews = Review.objects.all()
+    data = seriallizers.ReviewListSerializer(reviews, many=True).data
+    return Response(
+        data=data
+    )
+
+@api_view(http_method_names=['GET'])
+def review_detail_api_fiew(request, id):
+    try:
+        review = Review.objects.get(id=id)
+    except Review.DoesNotExist:
+        return Response(data={'error': 'Review not found!'}, status=status.HTTP_404_NOT_FOUND)
+    data = seriallizers.ReviewDetailSerializer(review, many=False).data
+    return Response(data=data)
